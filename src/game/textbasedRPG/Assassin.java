528ff86fef479d4053046d54ddb8cb09c9aebb7c
Timur Tillabaev <tillabaev.tim@gmail.com>
2025-06-10 21:54:09 -0700

    Changed multiplier calculations

--- src/game/textbasedRPG/Assassin.java
+++ src/game/textbasedRPG/Assassin.java
@@ -41,7 +41,7 @@ public class Assassin extends Rogue {
 			int damage = (int) (Math.random()*(this.getMaxDmg()-this.getMinDmg()+1)
 					+this.getMinDmg()); 
 			//multiplies damage by random variable (like rogue superclass crit but better)
-			damage *= Math.random()*2+2;
+			damage *= Math.random()*2+2.5;
 			damage = (int) damage;
 			damage = monster.takeDamage(damage);
 			if (monster.getIsAlive()) {
